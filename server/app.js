f4b03200d74b1d0ca956f219e35b846a64d40809
vi xent <alagovincent03@gmailcom>
2023-08-07 05:47:53 -0700

    done

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 
+
 app.get('/', (req, res) => {
     res.send("Server is alive");
 });
@@ -9,16 +10,17 @@ app.get('/hello', (req, res) => {
     res.send("Hello, my friend!");
 });
 
-app.get(['/goodbye', '/goodbye/*'], (req, res) => {
-    res.send("Goodbye, my friend!");
+
+app.get('/goodbye/until/forever', (req, res) => {
+    res.send("So long. Farewell. Have a great life!");
 });
 
 app.get('/goodbye/until/:time', (req, res) => {
     res.send(`Goodbye. See you ${req.params.time}.`);
 });
 
-app.get('/goodbye/until/forever', (req, res) => {
-    res.send("So long. Farewell. Have a great life!");
+app.get(['/goodbye', '/goodbye/*'], (req, res) => {
+    res.send("Goodbye, my friend!");
 });
 
 const port = 5000;
